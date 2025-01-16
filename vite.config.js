eb9e1a1ec2a31c5f99577b12ba536899f29c9b14
Dinesh <dineshcivilians6@gmail.com>
2025-01-16 19:46:47 +0530

    Fix heroicons import issue in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["@heroicons/react"], // externalize the package
+      output: {
+        globals: {
+          "@heroicons/react": "HeroIconsReact", // externalize as a global variable (if applicable)
+        },
+      },
+    },
+  },
 });
